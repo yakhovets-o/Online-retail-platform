d56a1ca000fdd891206026eee3686ba5bbe88adb
yakhovetso <yakhovetso@gmail.com>
2025-08-14 00:16:22 +0300

    add urls for app

---
+++ core/apps/users/urls.py
@@ -0,0 +1,21 @@
+from django.urls import re_path, include
+
+
+"""
+Available endpoints
+
+- /users/
+- /users/me/
+- /users/resend_activation/
+- /users/set_password/
+- /users/reset_password/
+- /users/reset_password_confirm/
+- /users/set_username/
+- /users/reset_username/
+- /users/reset_username_confirm/
+"""
+
+urlpatterns = [
+    re_path(r"^auth/", include("djoser.urls")),
+    re_path(r"^auth/", include("djoser.urls.authtoken")),
+]
